a0ba96ec9f2fdc3b72d0d5ea568a6517b071e61d
Jacek Pietrzyk <jacpie3k@gmail.com>
2019-04-25 08:53:06 +0200

    Update specs

--- lib/data/finder.rb
+++ lib/data/finder.rb
@@ -2,7 +2,7 @@
 
 require_relative 'local_data'
 
-# Finds in local storage
+# Finds campaigns in local storage
 class Finder
   def find_by_reference(reference)
     local_data.find { |item| item['reference'] == reference }
--- spec/lib/data/finder_spec.rb
+++ spec/lib/data/finder_spec.rb
@@ -3,19 +3,49 @@
 require 'spec_helper'
 require 'data/finder'
 describe Finder do
-  let(:proper_item) do
-    {
-      'reference' => '1',
-      'status' => 'enabled',
-      'description' => 'Description for campaign 11'
-    }
+  let(:reference) { '1' }
+  let('campaigns') do
+    [
+      {
+        'reference' => '1',
+        'status' => 'enabled',
+        'description' => 'Description for campaign 11'
+      },
+      {
+        'reference' => '2',
+        'status' => 'disabled',
+        'description' => 'Other description for campaign'
+      }
+    ]
   end
 
-  subject { described_class.new }
-
   describe '.find_by_reference' do
-    it 'returns parsed ads data' do
-      expect(subject.find_by_reference('1')).to eq(proper_item)
+    subject { described_class.new.find_by_reference(reference) }
+
+    context 'when local data is empty' do
+      it 'returns nil' do
+        stub_const('LocalData::CAMPAIGNS', [])
+        expect(subject).to eq(nil)
+      end
+    end
+
+    context 'when local data is present' do
+      before do
+        stub_const('LocalData::CAMPAIGNS', campaigns)
+      end
+
+      context 'and campaign with reference exists' do
+        it 'returns reference' do
+          expect(subject).to eq(campaigns.first)
+        end
+      end
+
+      context "and campaign with reference doesn't exists" do
+        let(:reference) { '3' }
+        it 'returns nil' do
+          expect(subject).to eq(nil)
+        end
+      end
     end
   end
 end
